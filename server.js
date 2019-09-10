debebdd7dda852f4ab9a971cb8f88a5bcb65cb4b
Jesus Rivera <r.jesus9107@gmail.com>
2019-09-10 14:16:51 -0700

    added functions & updated to two players

--- server.js
+++ server.js
@@ -10,8 +10,20 @@ let clients = [];
 
 io.on('connection', socket => {
 
+  clients.push(socket.id);
+
+  if (clients.length >= 2) {
+    socket.emit("begin");
+    socket.broadcast.emit("begin")
+  }
+
   socket.on('update', data => {
     socket.broadcast.emit('scores', {score: data['score'], player: data['player']})
+  });
+  
+  socket.on("disconnect", () => {
+    let i = clients.indexOf(socket.id);
+    clients.splice(i, 1);
   });
 
 });
